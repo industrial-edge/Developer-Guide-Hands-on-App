42a36df529a55f1a7498f27b6453c2f06ba32131
Chris1612 <65127248+Chris1612@users.noreply.github.com>
2021-05-03 08:12:55 +0200

    feat: IE App for Industrial Edge Developer Guide Release V1.2.1 (#1)
    * Release IE App for Industrial Edge Developer Guide V1.2.1
    
    Co-authored-by: Christopher Meyer <christopher.meyer@siemens.com>

---
+++ src/solution/my_edge_app/data-analytics/program/app.py
@@ -0,0 +1,38 @@
+# Copyright 2021 Siemens AG
+# This file is subject to the terms and conditions of the MIT License.  
+# See LICENSE file in the top-level directory.
+
+''' Main python module for Data Analytics service '''
+import time
+import os
+import sys
+import logging
+import data_analytics
+
+MAIN_LOOP_SLEEP_TIME = 0.5
+
+
+def main():
+
+    """ Initialize data-analytics """
+    
+    # configures basic logger
+    logger = logging.getLogger( __name__ )
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    handler.setFormatter(formatter)    
+    logger.addHandler(handler)
+
+
+    logger.info('Starting data-analytics service ...')
+    analytics = data_analytics.DataAnalyzer(logger.name)
+    analytics.handle_data()
+    
+    while True:
+      time.sleep(MAIN_LOOP_SLEEP_TIME)
+
+
+if __name__ == '__main__':
+    main()
